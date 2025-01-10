5fdcfeb0d9446861a76a93785668e75741549dd0
Tayyab-Younas <tabahis700@gmail.com>
2025-01-10 14:37:17 +0500

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,7 +3,7 @@ const about = () => {
     <>
       <div className="m-20 relative text-white font-sans">
         <img className="w-[60%]" src="./src/assets/about-img.png" alt="" />
-        <div className="absolute p-9 top-28 left-[51%] w-[30%] h-[60%] bg-blue-800">
+        <div className="absolute p-9 top-24 left-[51%] w-[30%] h-[66%] bg-blue-800 lg:text-xs">
           <h1 className="font-medium text-5xl mb-9">About us</h1>
           <p className="mb-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum aperiam quasi enim magni inventore fuga eius odio perspiciatis optio commodi! Quo eius dolorum ipsam. Facilis assumenda aliquid veniam fuga nulla.</p>
           <p className="mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere tenetur harum temporibus voluptas quam consectetur eveniet quae eius nemo laborum, illum, totam excepturi sed, dolorem provident porro earum inventore! Culpa.</p>
