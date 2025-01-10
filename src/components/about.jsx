347210c2e930a0e181b57a2e6d17fd229445378a
Tayyab-Younas <tabahis700@gmail.com>
2025-01-10 14:42:57 +0500

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,8 +5,10 @@ const about = () => {
         <img className="w-[60%]" src="./src/assets/about-img.png" alt="" />
         <div className="absolute p-9 top-24 left-[51%] w-[30%] h-[66%] bg-blue-800 lg:text-xs">
           <h1 className="font-medium text-5xl mb-9">About us</h1>
-          <p className="mb-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum aperiam quasi enim magni inventore fuga eius odio perspiciatis optio commodi! Quo eius dolorum ipsam. Facilis assumenda aliquid veniam fuga nulla.</p>
-          <p className="mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere tenetur harum temporibus voluptas quam consectetur eveniet quae eius nemo laborum, illum, totam excepturi sed, dolorem provident porro earum inventore! Culpa.</p>
+          <p>For more than 30 years we have been delivering world-class construction and we’ve built many lasting relationships along the way. 
+            </p>
+          <p>
+          We've matured into an industry leader and trusted resource for those seeking quality, innovation and reliability when building in the U.S.</p>
           <button className="bg-white p-2 rounded font-medium text-blue-700 font-sans text-lg">More on Our History</button>
         </div>
       </div>
